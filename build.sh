c08f3e747b6123c21f929deb331700475794295b
Noah Kim <noahbkim@gmail.com>
2017-05-12 19:34:09 -0700

    Started on redesign.

--- build.sh
+++ build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-if [[ "$1" != "" ]]; then
-    DIR="$1"
+# Read config file
+config="config.ini"
+url=$(awk '/url/ {print $2}' $config)
+branch=$(awk '/branch/ {print $2}' $config)
+commit=$(awk '/commit/ {print $2}' $config)
+location=$(awk '/location/ {print $2}' $config)
+
+
+# Check whether a directory is a git repository
+is_git_repository() {
+  if [ ! -d $1 ]; then return 1; fi
+  local result=1
+  pushd $1 > /dev/null 2>&1
+  if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then result=0; fi
+  popd > /dev/null 2>&1
+  return $result;
+}
+
+
+if is_git_repository "/tmp/bootstrap"; then
+  echo "Repository"
 else
-    DIR="bootstrap"
+  echo "Not repository"
 fi
 
-cp scss/* "$DIR/scss/"
-cd $DIR
-grunt dist
-
-rm -rf ../dist/
-mv dist/ ..
