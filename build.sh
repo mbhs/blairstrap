1890e572a2c744d0e094f692fa5c88ee43dad5f6
Noah Kim <noahbkim@gmail.com>
2017-05-13 12:36:27 -0400

    Added proper repository checking.

--- build.sh
+++ build.sh
@@ -10,18 +10,35 @@ location=$(awk '/location/ {print $2}' $config)
 
 # Check whether a directory is a git repository
 is_git_repository() {
+
+  # Check directory
   if [ ! -d $1 ]; then return 1; fi
+  
+  # Check rev-parse and remote url
   local result=1
   pushd $1 > /dev/null 2>&1
-  if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then result=0; fi
+  if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
+    if [ $(git config --get remote.origin.url) = $2 ]; then
+      result=0;
+    else result=1; fi
+  else result=2; fi
+
   popd > /dev/null 2>&1
   return $result;
+  
 }
 
-
-if is_git_repository "/tmp/bootstrap"; then
-  echo "Repository"
+echo "Checking $location is $url"
+if is_git_repository $location $url; then
+  echo "Correct repository and URL."
 else
-  echo "Not repository"
+  echo "Invalid repository or URL."
+  if [ -d $location ]; then
+    read -p "Overwrite files at $location? [Y/n]: " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      rm -r $location
+    else exit 1; fi
+  fi
+  git clone $url $location
 fi
-
